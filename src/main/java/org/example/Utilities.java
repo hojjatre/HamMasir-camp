03e145bbc248715576fbe4e2d8f6c708e8c40885
hojjatre <hojjat7878.h@gmail.com>
2023-08-17 00:23:26 +0330

    Change Utilities class

--- src/main/java/org/example/Utilities.java
+++ src/main/java/org/example/Utilities.java
@@ -1,46 +1,58 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utilities {
+    private List<Account> accounts;
+    private List<Transaction> transactions;
 
-    private List<Thread> threads = new ArrayList<>();
-    private int numberThread;
-    private int chunkSize;
+    public List<Account> readAccounts(){
+        BufferedReader accountReader;
+        String row = "";
 
-    BalancingThread balancingThread;
-    public void createThread(int chunkSize, int sizeData){
-        this.chunkSize = chunkSize;
-        this.numberThread = (int) Math.ceil(sizeData/chunkSize) + 1;
-    }
-
-    public CustomThreadPool splitMapByThreadNum(Map<Integer, Transaction> transactionMap, HashMap<Integer, Long> balance){
-        // Split Map to equal CHUNKs
-        List<Map<Integer, Transaction>> chunks = transactionMap.entrySet()
-                .stream().collect(Collectors.groupingBy(entry -> (entry.getKey() - 1) / this.chunkSize))
-                .values().stream()
-                .map(list -> list.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
-                .collect(Collectors.toList());
-        balancingThread = new BalancingThread(chunks, balance);
-        //Create our Threads
-        CustomThreadPool customThreadPool = new CustomThreadPool(numberThread);
-        System.out.println(numberThread);
-        for (int i = 0; i < numberThread; i++) {
-            customThreadPool.submitTask(balancingThread);
-//            System.out.println(i);
+        try {
+            accountReader = new BufferedReader(new FileReader("C:\\Users\\hojja\\IdeaProjects\\Thread_exercise\\src\\main\\java\\org\\example\\Race Conditions - Accounts.csv"));
+            while (( row = accountReader.readLine()) != null){
+                String[] data = row.split(",");
+                if (data[0].equals("ID")) continue;
+                Long amount = Long.parseLong(data[2].replace("\"", "") +  data[3].replace("\"", "") + data[4].replace("\"", ""));
+                accounts.add(
+                        new Account(Integer.parseInt(data[0]), data[1], amount));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return customThreadPool;
 
-//        customThreadPool.shutdown();
-//        for (int i = 0; i < numberThread; i++) {
-//            Thread thread = new Thread(balancingThread, String.valueOf(i));
-//            threads.add(thread);
-//        }
-//        return threads;
+        return accounts;
     }
 
-    public BalancingThread getBalancingThread() {
-        return balancingThread;
+    public List<Transaction> readTransaction(){
+        BufferedReader transactionReader;
+        String row = "";
+
+
+        try {
+            transactionReader = new BufferedReader(new FileReader("C:\\Users\\hojja\\IdeaProjects\\Thread_exercise\\src\\main\\java\\org\\example\\Race Conditions - Transaction.csv"));
+            while (( row = transactionReader.readLine()) != null){
+                String[] data = row.split(",");
+                if (data[0].equals("FromAccount")) continue;
+                Long amount = Long.parseLong(data[2].replace("\"", "") + "" +  data[3].replace("\"", ""));
+                transactions.add(new Transaction(accounts.get(Integer.parseInt(data[0]) - 1),
+                        accounts.get(Integer.parseInt(data[1]) - 1), amount));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return transactions;
     }
 }
